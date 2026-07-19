a3c4d0d8e8c6f074a79887d74cd8d4fe3123a2b9
Sathvik1533V <24r21a05hr@mlrit.ac.in>
2026-07-19 15:31:16 +0530

    feat(traversal)  to PrintAlternate arrays elements
    Added comments to describe the pattern and problem statement.

---
+++ 01-Arrays/02-Traversal/PrintAlternate.java
@@ -0,0 +1,14 @@
+/*
+Pattern : Traversal
+
+Problem : Print PrintAlternate Elements
+
+Difficulty : Easy
+*/
+public class PrintAlternate {
+  public static void main(String a[]) {
+    int[] arr = {11, 7, 25, 4, 18, 9};
+    for (int i = 0; i <arr.length; i+=2) {
+      System.out.println(arr[i]);
+}
+} 
