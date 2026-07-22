9cd892ceed6516e20d6a501e7df7fcc64d46a5c1
J-Pravalika <jpravalika76@gmail.com>
2026-07-22 20:38:08 +0530

    printing elements after index K, getting better at this

---
+++ 01-Arrays/02-Traversal/Phase-C/AfterIndexK.java
@@ -0,0 +1,9 @@
+public class AfterIndexK{ //K=2
+    public static void main(String a[]){
+        int[] arr = {8, 15, 3, 21, 6, 11, 18, 5, 9, 14};
+        int k=2;
+        for(int i=k+1;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ 01-Arrays/02-Traversal/Phase-C/FirstKReverse.java
@@ -0,0 +1,9 @@
+public class FirstKReverse{ //K=5
+    public static void main(String a[]){
+        int[] arr = {8, 15, 3, 21, 6, 11, 18, 5, 9, 14};
+        int k=5;
+        for(int i=k-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
