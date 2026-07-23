1d9c3920c3dd726532451db094233ee28aa6e609
J-Pravalika <jpravalika76@gmail.com>
2026-07-23 14:58:04 +0530

    organized accumulation with Phase-A folder

--- 01-Arrays/02-Traversal/Phase-C/AfterIndexK.java
+++ 01-Arrays/02-Traversal/Phase-C/AfterIndexK.java
@@ -7,3 +7,84 @@ public class AfterIndexK{ //K=2
         }
     }
 }
+public class PrintEveryK{ //K=3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public static void main(String a[]){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    int[] arr = {8, 15, 3, 21, 6, 11, 18, 5, 9, 14};
+
+
+
+
+
+
+
+    int l=2;r=
+
+
+
+
+
+
+
+    for(int i=0;i<arr.length;i+=3){
+
+
+
+
+
+
+
+      System.out.println(arr[i]);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+}
